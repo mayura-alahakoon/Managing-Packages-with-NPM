6c537f467284bb85e0f896e237a8178ef9670048
mayura-alahakoon <mayuraalahakoon@gmail.com>
2022-12-13 23:45:06 +0530

    added .env file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config();
 
 console.log("Hello World");
 
@@ -12,14 +12,26 @@ app.get("/", function(req, res) {
 
 
 
-  app.use(express.static(__dirname + "/public/"));
-  app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/"));
+app.use("/public", express.static(__dirname + "/public"));
   
-  app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html")
-  });
+app.get("/", function(req, res) {
+res.sendFile(__dirname + "/views/index.html")
+});
 
 
+app.get("/json", function(req, res) {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json(
+            {"message": "HELLO JSON"}
+        )
+    } else {
+        res.json(
+            {"message" : "Hello json"}
+        )
+    }
+
+});
 
 
 
